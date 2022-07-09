7ab1adae4bf9814fa5995d182988ae92578a5c30
kashiash <kashiash@gmail.com>
2022-07-09 13:44:16 +0200

    Initial Commit

---
+++ NBPCurrencyRates/NBPCurrencyRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBPCurrencyRatesApp.swift
+//  NBPCurrencyRates
+//
+//  Created by Jacek Placek on 09/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct NBPCurrencyRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
